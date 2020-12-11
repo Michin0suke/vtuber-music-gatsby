01ce1da9d94fbb6622296a97f73e93156223fc85
michinosuke <michiya.318.3738@gmail.com>
2020-12-12 01:03:36 +0900

    gatsby-plugin-google-analyticsの追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Vtuber Music`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Vtuber Musicは、Vtuberの歌ってみた動画をまとめたサイトです。`,
+    author: `@VtuberMusicCom`,
   },
   plugins: [
     `gatsby-plugin-postcss`,
@@ -37,6 +37,35 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "G-1FXR3XGN8L",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: false,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        // optimizeId: "",
+        // Enables Google Optimize Experiment ID
+        // experimentId: "",
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: "0",
+        // Defers execution of google analytics script after page load
+        defer: true,
+        // Any additional optional fields
+        sampleRate: 100,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "vtuber-music.com",
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
