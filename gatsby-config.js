c83bb6b273cfe20508649691db1229e90a5aeeac
michinosuke <michiya.318.3738@gmail.com>
2020-12-12 15:45:55 +0900

    google analytics v3に変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "G-1FXR3XGN8L",
+        trackingId: "UA-185387868-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Setting this parameter is optional
