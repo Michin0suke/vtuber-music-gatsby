f1de2124863743796aefbe190f168a97791e6b11
michinosuke <michiya.318.3738@gmail.com>
2021-01-11 23:05:24 +0900

    apolloにfetchを追加

--- src/queries/client.js
+++ src/queries/client.js
@@ -1,7 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import fetch from 'cross-fetch';
 
 export const client = new ApolloClient({
-  uri: 'https://api.vtuber-music.com/graphql',
+  uri: new HttpLink({ uri: 'https://api.vtuber-music.com/graphql', fetch }),
+  // uri: 'https://api.vtuber-music.com/graphql',
   // uri: 'http://vtuber-music.test/graphql',
   cache: new InMemoryCache()
 });
