719e85c193c45316cfa42a84e8e4391d48ddd36a
michinosuke <michiya.318.3738@gmail.com>
2021-01-13 06:07:34 +0900

    update assets and api

--- src/components/profileImage.js
+++ src/components/profileImage.js
@@ -9,7 +9,7 @@ export default ({ artist, href, className, hoverEffect }) => {
     return (
         <div className={`relative bg-white rounded-full shadow overflow-hidden ${className}`}>
             <ProfileImg color='#999' className='absolute top-0 left-0 right-0 bottom-0 m-auto w-3/5'/>
-            { artist.profile_image?.childImageSharp?.fluid &&
+            { artist?.profile_image?.childImageSharp?.fluid &&
                 <Img fluid={artist.profile_image.childImageSharp.fluid} alt={`${artist.name}さんのプロフィール画像`}/> }
             <div className={`absolute top-0 left-0 w-full h-full opacity-20 ${hoverEffect && 'sm:hover:bg-white'}`}/>
             {href && 
