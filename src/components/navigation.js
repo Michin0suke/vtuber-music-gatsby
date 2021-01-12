f7d9fdb13ba2d2e325f04dfc9950441e30468fb2
michinosuke <michiya.318.3738@gmail.com>
2021-01-13 01:18:42 +0900

    singers[0]が存在しない場合のクラッシュへの対処

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,7 +2,7 @@
     <p style={{marginBottom: 5, paddingLeft: 10}}>
     <span><Link to={`/videos`} style={{color: "#555", textDecoration: 'none'}}>動画</Link></span>
     <span style={{ padding: '0 5px' }}>{'>'}</span>
-    <span><Link to={`/artist/${video.singers[0].id}`} style={{color: "#555", textDecoration: 'none'}}>{video.singers[0].name}</Link></span>
+    <span><Link to={`/artist/${video.singers[0]?.id}`} style={{color: "#555", textDecoration: 'none'}}>{video.singers[0]?.name}</Link></span>
     <span style={{ padding: '0 5px' }}>{'>'}</span>
     <span>{video.music.title}</span>
     </p>
--- src/components/videoCard.js
+++ src/components/videoCard.js
@@ -12,12 +12,12 @@ export const VideoCardHeader = ({ video, className }) => (
             <div className='pr-10'>
                 <div className='absolute top-0 right-0 w-full h-full rounded-full bg-white opacity-50'/>
                 <div className='flex relative'>
-                    <Link to={`/artist/${video.singers[0].id}`} className='relative'>
+                    <Link to={`/artist/${video.singers[0]?.id}`} className='relative'>
                         <ProfileImg artist={video.singers[0]} className='w-12 h-12 w-16 h-16' hoverEffect/>
                     </Link>
                     <div className='ml-3'>
                         <h4 className='py-1 text-lg text-black font-bold'>{video.music.title}</h4>
-                        <Link to={`/artist/${video.singers[0].id}`}>
+                        <Link to={`/artist/${video.singers[0]?.id}`}>
                             <p className='text-md text-black sm:hover:text-black'>{video.singers.map(singer => singer.name).join(' & ')}</p>
                         </Link>
                     </div>
@@ -31,7 +31,7 @@ export default ({ video, className, withPublishDate }) => (
     <article className={`relative md:rounded overflow-hidden ${className}`}>
         <VideoThumbnail to={`/video/${video.id}`} video={video} className='mb-2' withHoverLink/>
         <div className='z-10 flex'>
-            <Link to={`/artist/${video.singers[0].id}`} className='relative'>
+            <Link to={`/artist/${video.singers[0]?.id}`} className='relative'>
                 <ProfileImg artist={video.singers[0]} className='mx-2 w-14 h-14' hoverEffect/>
             </Link>
             <div className='w-full'>
@@ -41,7 +41,7 @@ export default ({ video, className, withPublishDate }) => (
                     </h2>
                 </Link>
                 <div className='flex justify-between items-center sm:block pr-2'>
-                    <Link to={`/artist/${video.singers[0].id}`}>
+                    <Link to={`/artist/${video.singers[0]?.id}`}>
                         <p className='text-sm text-gray-500 sm:hover:text-black'>{video.singers.map(singer => singer.name).join(' & ')}</p>
                     </Link>
                     {withPublishDate && <time className='text-xs text-gray-500' dateTime={video.release_date}>{video.release_date?.replace(/-/g, '/')}</time>}
