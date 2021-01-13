6a697172e2171ebab7dc505878f63497251df718
michinosuke <michiya.318.3738@gmail.com>
2021-01-14 05:58:38 +0900

    validateの変更

--- src/utils/validateUrl.js
+++ src/utils/validateUrl.js
@@ -12,7 +12,7 @@ const validateChannelUrl = (rawUrl) => {
 
     const pattern_new = /^https:\/\/www.youtube.com\/(c\/)?([\w-%]+)/
     if (url.match(pattern_new)) {
-        console.log(JSON.stringify(url.match(pattern_new), null, 2))
+        // console.log(JSON.stringify(url.match(pattern_new), null, 2))
         if (url.match(pattern_new)[2] === 'watch') return false
         return url.match(pattern_new)[2]
     }
@@ -21,8 +21,8 @@ const validateChannelUrl = (rawUrl) => {
 }
 
 const validateVideoUrl = (url) => {
-    if (url.match(/^https:\/\/www.youtube.com\/watch\?v=([\w-]{11})(&.*)?$/)) {
-        return url.match(/^https:\/\/www.youtube.com\/watch\?v=([\w-]{11})(&.*)?$/)[1]
+    if (url.match(/^https:\/\/www.youtube.com\/watch\?(\w+=\w+&)?v=([\w-]{11})(&.*)?$/)) {
+        return url.match(/^https:\/\/www.youtube.com\/watch\?(\w+=\w+&)?v=([\w-]{11})(&.*)?$/)[2]
     }
     if (url.match(/^https:\/\/youtu.be\/([\w-]{11})$/)) {
         return url.match(/^https:\/\/youtu.be\/([\w-]{11})$/)[1]
@@ -35,16 +35,22 @@ module.exports = {
     validateVideoUrl,
 }
 
-const arr = [
-    'https://www.youtube.com/channel/UC5CwaMl1eIgY8h02uZw7u8A',
-    'https://www.youtube.com/c/hiroyukix',
-    'https://www.youtube.com/hiroyukix',
-    'https://www.youtube.com/user/KIYOisGOD',
-    'https://www.youtube.com/c/%E5%A4%A9%E4%BD%BF%E3%82%BB%E3%82%A6',
-    'https://www.youtube.com/c/あいうえお'
+// const channelURLs = [
+//     'https://www.youtube.com/channel/UC5CwaMl1eIgY8h02uZw7u8A',
+//     'https://www.youtube.com/c/hiroyukix',
+//     'https://www.youtube.com/hiroyukix',
+//     'https://www.youtube.com/user/KIYOisGOD',
+//     'https://www.youtube.com/c/%E5%A4%A9%E4%BD%BF%E3%82%BB%E3%82%A6',
+//     'https://www.youtube.com/c/あいうえお'
 
-].map((url) => validateChannelUrl(url)).join('\n')
+// ].map((url) => validateChannelUrl(url)).join('\n')
 
-console.log('')
+// const videoURLs = [
+//     'https://youtu.be/qZn8DAM9H5Y',
+//     'https://www.youtube.com/watch?v=j1hft9Wjq9U&ab_channel=THEFIRSTTAKE',
+//     'https://www.youtube.com/watch?app=desktop&v=gNRp28pKE5A&t=17s&ab_channel=%E7%B4%85%E6%9D%AF%E3%81%B2%E3%81%90%E3%82%8C',
+// ].map(url => validateVideoUrl(url)).join('\n')
 
-console.log(arr)
+// console.log(channelURLs)
+// console.log()
+// console.log(videoURLs)
